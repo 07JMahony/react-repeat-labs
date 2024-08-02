20b34b0bedadb67de16e759991a384b0952bb5c2
John O'Mahony <07jmahony@gmail.com>
2024-08-02 20:09:14 +0100

    New button for upcoming movies page

--- moviess/src/pages/upcomingMoviesPage.js
+++ moviess/src/pages/upcomingMoviesPage.js
@@ -4,7 +4,7 @@ import { getUpcomingMovies } from "../api/tmdb-api";
 import Spinner from '../components/spinner';
 import { useQuery } from 'react-query';
 import { getMovies } from "../api/tmdb-api";
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import PlaylistAddIcon from "../components/cardIcons/addToPlaylist";
 
 
 
@@ -13,15 +13,7 @@ const UpcomingMoviesPage = () => {
 
 
   const [movies, setMovies] = useState([]);
-  const favorites = movies.filter(m => m.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
 
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
 
   useEffect(() => {
     // Fetch upcoming movies from the TMDB API.
@@ -35,7 +27,7 @@ const UpcomingMoviesPage = () => {
       title='Upcoming Movies'
       movies={movies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />
+        return <PlaylistAddIcon movie={movie} />
       }}
     />
   );
